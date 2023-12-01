d1620722f4783a9d4f49ed06bc7a42089b2cd232
gnwodeyar1312 <gnwodeyar@gmail.com>
2023-12-01 20:19:14 +0530

    added the data requierd for Horoscope Generator

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-const data1 = ['']
+// Horoscope Generator
+
+const horoscopeData = {
+    signs: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'],
+    traits: [
+        ['Energetic', 'Courageous', 'Confident'],
+        ['Patient', 'Practical', 'Dependable'],
+        ['Adaptable', 'Curious', 'Eloquent'],
+        ['Intuitive', 'Compassionate', 'Protective'],
+        ['Creative', 'Passionate', 'Dignified'],
+        ['Analytical', 'Kind', 'Hardworking'],
+        ['Diplomatic', 'Harmonious', 'Charming'],
+        ['Resourceful', 'Brave', 'Determined'],
+        ['Optimistic', 'Adventurous', 'Philosophical'],
+        ['Responsible', 'Disciplined', 'Cautious'],
+        ['Innovative', 'Humanitarian', 'Independent'],
+        ['Imaginative', 'Compassionate', 'Intuitive']
+    ],
+    predictions: [
+        'You may find new opportunities in your career. Embrace change and take calculated risks.',
+        'Focus on your relationships today. A conversation may lead to a new understanding.',
+        'Your creativity will shine today. Explore new hobbies or activities.',
+        'Trust your instincts today. Your inner wisdom will guide you in the right direction.',
+        'Express your passions openly. Your enthusiasm will inspire others.',
+        'Stay focused on your tasks today. Your hard work will pay off soon.',
+        'Seek harmony in your interactions today. Your diplomatic approach will bring positive outcomes.',
+        'Embrace challenges courageously. Your determination will lead to success.',
+        'Be open to new experiences. An adventure awaits you today.',
+        'Maintain your discipline and responsibility. It will bring you long-term benefits.',
+        'Your uniqueness is your strength. Embrace your individuality today.',
+        'Listen to your intuition. It will provide valuable insights.'
+    ],
+}
+
+const  getHoroscopeData = (data) => {
+
+}
